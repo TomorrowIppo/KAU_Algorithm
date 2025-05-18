53cf7280b93158d407973203d4bb6cc05c399de3
TomorrowIppo <bjk1624@gmail.com>
2025-05-18 13:43:55 +0900

    feat: BOJ_1446 (지름길) - dijkstra를 이용하여 구현. 입력으로 주어진 길 제외한 나머지는 기본적으로 길이가 1인 지름길로 이어져있다 가정.

---
+++ KAU_KOALA_18th/Week7/MockTest/BOJ_1446/BOJ_1446.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define W first
+#define V second
+
+using namespace std;
+
+constexpr int MAX = 10001;
+constexpr int INF = 987654321;
+
+int V, E, st;
+vector<pair<int, int>> graph[MAX];  // { Weight, Value }
+int dist[MAX];
+
+void input();
+void solve();
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+void input() 
+{
+    // input & init dist
+    cin >> E >> V;
+    fill(dist, dist + V + 1, INF);
+
+    for(int i = 0; i <= V - 1; i++)
+        graph[i].push_back({1, i + 1});
+    
+    while(E--) 
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({w, v});
+        st = min(st, u);
+    }
+}
+
+void solve()
+{
+    priority_queue<pair<int, int>,
+                   vector<pair<int, int>>,
+                   greater<pair<int, int>>> pq;
+    dist[0] = 0;
+    pq.push({dist[st], st});
+    while(!pq.empty())
+    {
+        auto cur = pq.top(); pq.pop();
+
+        if(dist[cur.V] != cur.W) continue;
+
+        for(auto nxt : graph[cur.V])
+        {
+            if(dist[nxt.V] <= dist[cur.V] + nxt.W) continue;
+
+            dist[nxt.V] = dist[cur.V] + nxt.W;
+            pq.push({dist[nxt.V], nxt.V});
+        }
+    }
+
+    // for(int i = 1; i <= V; i++)
+    // {
+    //     if(dist[i] == INF) continue;
+    //     else cout << i << " : " << dist[i] << '\n';
+    // }
+    cout << dist[V] << '\n';
+}
